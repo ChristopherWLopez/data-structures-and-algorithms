7deb453453aba85109fef80924fe8814b0b9ea67
christopher lopez <christopherwlopez89@gmail.com>
2023-01-06 09:45:12 -0800

    added tests

--- javascript/linked.list/__tests__/linked-lists.test.js
+++ javascript/linked.list/__tests__/linked-lists.test.js
@@ -2,11 +2,12 @@
 
 const Node = require('../linked-list-implementation/node');
 const LinkedList = require('../linked-list-implementation/linked-lists');
+const { default: test } = require('node:test');
 // const { it } = require('node:test');
 // const { default: expect } = require('expect');
 
 
-describe('Linked list tests', ()=>{
+
 
     it('create a new head',()=>{
         const newList = new LinkedList()
@@ -22,15 +23,94 @@ describe('Linked list tests', ()=>{
         expect(ll).toEqual(true);
     })
 
-})
 
-describe('linked list insertion test', ()=>{
+    test("tests insertBefore: adds a node with a given new value immediately before the first node with the given value", () => {
 
-    it('tests inserBefore: add a node with a given new value before the first node with the given value',()=>{
 
+        // create new instance of a linked list
         let insertBeforeLinkedList = new LinkedList();
-        insertBeforeLinkedList.head({value: 1, next: { value:3, next: {value: 4, next: null } } })
-        let newList = insert
-        expect(newList).toEqual({value: 1, next: { value: 2, next: { value: 3, next: {value: 4, next:null},},},},
-    });
-});
+
+        // create the linked list (head)
+        insertBeforeLinkedList.head = {
+          value: 1,
+          next: { value: 3, next: { value: 4, next: null } },
+        };
+
+
+        // take  the new linked list and apply the method to it with the args
+        let newList = insertBeforeLinkedList.insertBefore(3, 2);
+        expect(newList).toEqual({
+          value: 1,
+          next: {
+            value: 2,
+            next: {
+              value: 3,
+              next: {
+                value: 4,
+                next: null,
+              },
+            },
+          },
+        });
+      });
+
+
+      test("tests insertAfter: adds a node with a given new value immediately after the first node with the given value", () => {
+        
+        let insertAfterLinkedList = new LinkedList();
+        insertAfterLinkedList.head = {
+          value: 1,
+          next: { value: 2, next: { value: 4, next: null } },
+        };
+        let newList = insertAfterLinkedList.insertAfter(2, 3);
+        expect(newList).toEqual({
+          value: 1,
+          next: {
+            value: 2,
+            next: {
+              value: 3,
+              next: {
+                value: 4,
+                next: null,
+              },
+            },
+          },
+        });
+      });
+
+      test("It returns the value of the node kth from the end of the list", () => {
+        let llKth = new LinkedList();
+        llKth.head = {
+          value: 1,
+          next: {
+            value: 2,
+            next: {
+              value: 3,
+              next: {
+                value: 4,
+                next: null,
+              },
+            },
+          },
+        };
+        expect(llKth.kthFromEnd(1)).toBe(3);
+      });
+
+      test("If given a k longer than the length of the list returns undefined", () => {
+        let llKthBad = new LinkedList();
+        llKthBad.head = {
+          value: 1,
+          next: {
+            value: 2,
+            next: {
+              value: 3,
+              next: {
+                value: 4,
+                next: null,
+              },
+            },
+          },
+        };
+        expect(llKthBad.kthFromEnd(10)).toBe(undefined);
+      });
+    
