e3ce202446fd5b790cb5a61d2b8e7279fe8b8d84
christopher lopez <christopherwlopez89@gmail.com>
2023-01-10 20:07:31 -0800

    adds git ignore and tests

---
+++ javascript/linked.list/__tests__/linked-zip.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const LinkedList = require("../linked-list-implementation/linked-lists");
+
+
+test("make sure nodes zip", ()=> {
+    let ll1 = new LinkedList();
+    let newList = new LinkedList();
+     ll1.head={
+        value:1,
+        next: {
+              
+            value: 3,
+            next: {
+                value: 2,
+                next:null
+            },
+        },
+    };
+    let ll2 = new LinkedList();
+    ll2.head = {
+        value: 5,
+        next: {
+            value: 8,
+            next: null
+        },
+
+        };
+
+         expect(LinkedList.zipList(newList.append(ll1,ll2)));
+        });
+
+
