56f99849ffea1ecdd47e4a45fd2817ceed0ec71f
christopher lopez <christopherwlopez89@gmail.com>
2023-04-03 21:01:22 -0700

    first methods

---
+++ javascript/graph/directedEdge.js
@@ -0,0 +1,8 @@
+class DirectedEdge{
+    constructor(vertex, weight=null){
+        this.vertex = vertex;
+        this.weight = weight
+    }
+}
+
+module.exports=DirectedEdge;
---
+++ javascript/graph/graph.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Vertex = require("./vertex");
+const Edge = require("./directedEdge");
+
+class Graph{
+    constructor(){
+        this.vertices = new Set();
+        this.edges = new Map();
+    }
+
+    addVertex(value){
+        let newVertex = new Vertex;
+        this.vertices.add(newVertex);
+        this.edges.set(newVertex, []);
+
+        return newVertex;
+
+    }
+
+    addEdge(start, end, weight){
+        if(!this.vertices.has(start) ||!this.vertices.has(end)) throw new Error("invalid vertices");
+
+        let newEdge = new DirectedEdge(end, weight);
+        let neighbors = this.edges.get(start);
+        neighbors.push(newEdge);
+    }
+}
+
+
+module.exports = Graph;
