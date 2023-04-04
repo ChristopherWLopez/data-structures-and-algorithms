c952ce2e4aa1e6ef69d9c7316b4953c7043dddc3
christopher lopez <christopherwlopez89@gmail.com>
2023-04-03 17:44:47 -0700

    working on tests

--- javascript/hashtable/hash-left-join/hashmap-left-join.test.js
+++ javascript/hashtable/hash-left-join/hashmap-left-join.test.js
@@ -2,19 +2,19 @@
 
 const leftJoin = require('./hashmap-left-join');
 
-describe('left joins',()=> {
-    test('returns array with key and like values or Null.',()=>{
+// describe('left joins',()=> {
+//     test('returns array with key and like values or Null.',()=>{
 
-        let hash1 = new Map();
-        hash1.set('cat', 'maru');
-        hash1.set('dog','paloma');
+//         let hash1 = new Map();
+//         hash1.set('cat', 'maru');
+//         hash1.set('dog','paloma');
 
-        let hash2 = new Map();
-        hash2.set('bark', 'woof');
-        hash2.set('dog', 'meow');
+//         let hash2 = new Map();
+//         hash2.set('bark', 'woof');
+//         hash2.set('dog', 'meow');
 
-        expecct(leftJoin(hash1, hash2)).toEqual([
-            ['cat', 'maru', nu]
-        ])
-    })
-})
+//         expecct(leftJoin(hash1, hash2)).toEqual([
+//             ['cat', 'maru', nu]
+//         ])
+//     })
+// })
