93a13686386ee3f4bcb6a216e81b96e9ba200516
christopher lopez <christopherwlopez89@gmail.com>
2023-08-14 19:12:55 -0700

    leetCode

---
+++ javascript/leetCode/twoSum/twoSum.js
@@ -0,0 +1,17 @@
+twoSums(mySums, myTarget);
+
+        function twoSum(nums, target) {
+  const numIndicesMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const completment = target - nums[i];
+    if (numIndicesMap.has(completment)) {
+      return [numIndicesMap.get(completment), i];
+    }
+    numIndicesMap.set(nums[i], i);
+  }
+
+  throw new Error('No solution found');
+}
+
+//comments for this merge
