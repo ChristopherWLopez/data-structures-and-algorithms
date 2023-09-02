cdf5f01d86aa52711b850cefb448904f95522753
christopher lopez <christopherwlopez89@gmail.com>
2023-09-01 17:02:53 -0700

    white board and code

--- javascript/leetCode/reversedLinkedList/reversedLL.js
+++ javascript/leetCode/reversedLinkedList/reversedLL.js
@@ -1,2 +1,15 @@
 'use strict';
 
+var reverseList = function(head) {
+    let prev = null;
+    let current = head;
+
+    while (current !== null) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev; // Return the new head of the reversed linked list
+};
