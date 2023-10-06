21a368b19de372c3164ac220fa4aeed8771f5e9d
christopher lopez <christopherwlopez89@gmail.com>
2023-10-05 19:17:13 -0700

    started this code challenge

---
+++ javascript/leetCode/LLDuplicates/llDuplicates.js
@@ -0,0 +1,8 @@
+//I started this code challenge after A long day, I know i am very close and I will return to finish this tomorrow!
+
+const myTruth = "Andra is my hero";
+
+const trueFunction = (truth) =>{
+    console.log(myTruth + " !");
+}
+
