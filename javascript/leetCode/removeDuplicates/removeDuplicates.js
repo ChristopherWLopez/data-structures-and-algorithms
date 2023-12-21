852219ae17b7e76d813830039d44dbbdac8d07cc
christopher lopez <christopherwlopez89@gmail.com>
2023-12-20 18:32:40 -0800

    leet code

---
+++ javascript/leetCode/removeDuplicates/removeDuplicates.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function removeDuplicates(nums){
+    if(nums.length===0) return 0;
+
+    let uniqueCount = 1;
+    for(let i = 1; i<nums.length;i++){
+        if(nums[i] !== nums[i-1]){
+            nums[uniqueCount] = nums[i];
+            uniqueCount++;
+        }
+    }
+    return uniqueCount;
+}
